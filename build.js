b5546828bcc392c1e4a9f91f3140fdc76e76234a
anderoonies <andrewbayer2016@u.northwestern.edu>
2021-11-11 17:05:34 -0800

    Simplify docs, build/dist

--- build.js
+++ build.js
@@ -1,10 +1,11 @@
 const esbuild = require('esbuild');
+const dist = process.argv?.[2] ?? false;
 
 const defaultConfig = {
     logLevel: 'info',
     bundle: true,
 };
-const configs = [
+let configs = [
     {
         ...defaultConfig,
         entryPoints: ['./entrypoints/chs.js'],
@@ -13,26 +14,31 @@ const configs = [
     },
     {
         ...defaultConfig,
-        entryPoints: ['./entrypoints/chs.js'],
-        outfile: './dist/chs.min.mjs',
-        format: 'cjs',
-        minify: true,
-    },
-    {
-        ...defaultConfig,
         entryPoints: ['./entrypoints/windowBinder.js'],
         outfile: './dist/chs.iife.js',
         format: 'iife',
         globalName: 'CHSJS',
     },
-    {
-        ...defaultConfig,
-        entryPoints: ['./entrypoints/windowBinder.js'],
-        outfile: './dist/chs.iife.min.js',
-        format: 'iife',
-        globalName: 'CHSJS',
-        minify: true,
-    },
 ];
+
+if (dist) {
+    configs = configs.concat([
+        {
+            ...defaultConfig,
+            entryPoints: ['./entrypoints/chs.js'],
+            outfile: './dist/chs.min.mjs',
+            format: 'cjs',
+            minify: true,
+        },
+        {
+            ...defaultConfig,
+            entryPoints: ['./entrypoints/windowBinder.js'],
+            outfile: './dist/chs.iife.min.js',
+            format: 'iife',
+            globalName: 'CHSJS',
+            minify: true,
+        },
+    ]);
+}
 
 configs.forEach(esbuild.build);
