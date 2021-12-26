8692b651b6c707e4d27b220bd76777422037b9bd
Andy Bayer <anderoonies@users.noreply.github.com>
2021-12-25 20:00:39 -0800

    Actually subclass Circle in the example for subclassing Circle (#92)

--- site/examples/graphics/circle/subclassing/subclassing.js
+++ site/examples/graphics/circle/subclassing/subclassing.js
@@ -1,37 +1,35 @@
 setBackgroundColor('gray');
 
 let flakeID = 0;
-class Snowflake {
+class Snowflake extends Circle {
     theta = Randomizer.nextFloat(Math.PI * 2);
     dTheta = 0.04;
 
     constructor(x, y, dy) {
+        super(Randomizer.nextInt(1, 3));
         this.id = flakeID++;
+        this.startX = x;
         this.x = x;
         this.y = y;
         this.dy = dy;
-
-        this.shape = new Circle(Randomizer.nextInt(1, 3));
-        this.shape.setColor('white');
-        this.shape.setPosition(x, y);
-        add(this.shape);
+        this.color = 'white';
         setTimer(this.update, 50, {}, this.id);
     }
 
     update = () => {
         this.y += this.dy;
-        const x = this.x - (Math.sin(this.theta) * getWidth()) / 2;
-        this.shape.setPosition(x, this.y);
+        const x = this.startX - (Math.sin(this.theta) * getWidth()) / 2;
+        this.setPosition(x, this.y);
 
         this.theta += this.dTheta;
 
         if (this.y > getHeight()) {
             stopTimer(this.id);
-            remove(this.shape);
+            remove(this);
         }
     };
 }
 
 setTimer(() => {
-    new Snowflake(Randomizer.nextInt(getWidth()), 0, Randomizer.nextInt(1, 3));
+    add(new Snowflake(Randomizer.nextInt(getWidth()), 0, Randomizer.nextInt(1, 3)));
 }, 10);
