787c41d6873107d812dda703fd65c48ff73bdd50
anderoonies <andrewbayer2016@u.northwestern.edu>
2022-01-11 21:36:25 -0800

    Some updates to the compositing examples

--- site/examples/graphics/compositing/compositing.md
+++ site/examples/graphics/compositing/compositing.md
@@ -4,8 +4,11 @@ layout: example
 code: compositing.js
 ---
 
-Setting the `globalCompositeOperation` of an element will cause that operation to be used before drawing it.
+Setting the `globalCompositeOperation` of the context will cause that operation mode to be used when drawing.
 The options can all be seen on [MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation).
 
 In this example, a subclassed circle with `globalCompositeOperation = 'destination-out'` is used to act as an "eraser".
+
+The only argument to the `draw` function is the 2d context, and modifying the `globalCompositeOperation` before invoking `super.draw` will modify the context for the duration of the `Circle`'s draw.
+
 When you click or tap the canvas, the circle's `globalCompositeOperation` becomes `destination-in`, and will act as whatever the opposite of an eraser is.
--- site/examples/graphics/compositingbuildings/compositingbuildings.js
+++ site/examples/graphics/compositingbuildings/compositingbuildings.js
@@ -23,29 +23,29 @@ class ClipPolygon extends Polygon {
 }
 
 const projectRectangle = (rectangle, vanishingPoint, isLeft = false) => {
-    let top = rectangle.y;
+    // go off the top a bit for subpixel stuff
+    let top = rectangle.y - 0.001;
     let left = rectangle.x;
     let right = rectangle.x + rectangle.width;
     if (isLeft) {
-        left = rectangle.x + rectangle.width;
-        right = rectangle.x;
+        // swap left and right since the vector will be pointing left
+        let temp = right;
+        right = left;
+        left = temp;
     }
     const v = vanishingPoint.clone().subtract(new Vector(left, top));
     if (isLeft) {
         v.multiply(-1, -1);
     }
 
-    let theta = toRad(v.heading() % 90);
-    theta = Math.abs(theta);
+    let theta = Math.abs(toRad(v.heading() % 90));
     const dy = rectangle.width * Math.atan(theta);
 
     const clip = new ClipPolygon();
     clip.setAnchor({ ...rectangle.anchor });
-    // go off the top a bit for subpixel stuff
-    clip.addPoint(left, top - 1);
-    clip.addPoint(right, top - 1);
-    clip.addPoint(right, top + dy - 1);
-    clip.setColor('red');
+    clip.addPoint(left, top);
+    clip.addPoint(right, top);
+    clip.addPoint(right, top + dy);
     return clip;
 };
 
