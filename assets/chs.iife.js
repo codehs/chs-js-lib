b68bb3e066d7e27db0ae3d4772e1af6fbc197ae3
anderoonies <anderoonies@users.noreply.github.com>
2022-01-26 01:11:44 +0000

    deploy: 5d3509b65485341f022abf5ee5fb9dd7bc7254a9

--- assets/chs.iife.js
+++ assets/chs.iife.js
@@ -1861,19 +1861,19 @@ var CHSJS = (() => {
         });
       }
       const x0 = Math.floor(x) % PERLIN_SIZE_2D;
-      const x1 = x0 + 1;
+      const x1 = (x0 + 1) % PERLIN_SIZE_2D;
       const y0 = Math.floor(y) % PERLIN_SIZE_2D;
-      const y1 = y0 + 1;
-      const dx = x - x0;
-      const dy = y - y0;
+      const y1 = (y0 + 1) % PERLIN_SIZE_2D;
+      const dx = (x - x0) % PERLIN_SIZE_2D;
+      const dy = (y - y0) % PERLIN_SIZE_2D;
       const gradientTL = perlin2[x0][y0];
       const gradientTR = perlin2[x1][y0];
       const gradientBL = perlin2[x0][y1];
       const gradientBR = perlin2[x1][y1];
-      const noiseTL = gradientTL.dot(x - x0, y - y0);
-      const noiseTR = gradientTR.dot(x - x1, y - y0);
-      const noiseBL = gradientBL.dot(x - x0, y - y1);
-      const noiseBR = gradientBR.dot(x - x1, y - y1);
+      const noiseTL = gradientTL.dot(dx, dy);
+      const noiseTR = gradientTR.dot(dx - 1, dy);
+      const noiseBL = gradientBL.dot(dx, dy - 1);
+      const noiseBR = gradientBR.dot(dx - 1, dy - 1);
       const xFade = fade(dx);
       return (lerp(lerp(noiseTL, noiseTR, xFade), lerp(noiseBL, noiseBR, xFade), fade(dy)) + 1) / 2;
     }
